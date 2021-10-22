850c0ba57137cc5ee7730751e97487d54468efc2
ngoquoctruong <ngoquocturong302@gmail.com>
2021-10-22 08:01:41 +0700

    first commit

---
+++ src/settings.385a7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/MainScene.fire",orientation:"portrait",jsList:["assets/scripts/server/gta-amplitude.f658d.js"],bundleVers:{internal:"620a7",resources:"d7596",main:"ede0e"}};
